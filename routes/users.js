150efa53cd63a6a89b42a1e7e24631f0c5c17659
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-09 22:07:43 -0700

    migrates work

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express')
-// const Users = require('./routes/user') 
+const Users = require('../models/userModel.js') 
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
+//Testing welcome message when hitting endpoint
 router.get('/', async(req, res) => {
     try {
         res.status(200).json({message: "Welcome!"})
@@ -9,5 +11,13 @@ router.get('/', async(req, res) => {
         res.status(500).json({error: "Cannot retrieve message"})
     }
 })
+
+//REGISTER post
+// router.post('/register', async(req, res) => {
+//     let user = req.body
+
+
+//     Users.
+// })
 
 module.exports = router
