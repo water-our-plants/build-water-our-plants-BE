19fe73b87f933c8734c7dcdf4e0f5d669010ca66
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-09 22:47:44 -0700

    register works

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,22 @@
-const db = require('../data/dbConfig.js')
+const db = require('../config/dbConfig.js')
 
 module.exports = {
     get,
+    add,
+    findById
 }
 
 function get() {
     return db('users')
+}
+
+async function add(user){
+    const [id] = await db('users').insert(user)
+    return findById(id)
+}
+
+function findById(id) {
+    return db('users')
+        .where({id})
+        .first()
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,20 @@ const express = require('express')
 const Users = require('../models/userModel.js') 
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken")
+const secrets = require('../config/secrets.js')
+
+function generateToken(user) {
+    const payload = {
+      id: user.id
+    };
+  
+    const signOptions = {
+        expiresIn:  "12h",
+    };
+  
+    return jwt.sign(payload, secrets.jwt, signOptions);
+  }
 
 //Testing welcome message when hitting endpoint
 router.get('/', async(req, res) => {
@@ -12,12 +26,30 @@ router.get('/', async(req, res) => {
     }
 })
 
-//REGISTER post
-// router.post('/register', async(req, res) => {
-//     let user = req.body
+// REGISTER post
+router.post('/register', async(req, res) => {
+    let user = req.body
 
+    //Encrypst the password
+    const hash = bcrypt.hashSync(user.password, 10)
+    user.password = hash
 
-//     Users.
-// })
+    await Users.add(user)
+        .then(saved => {
+            //Generates token
+            const token = generateToken(saved)
+            //decoded gets the user id of the user
+            let decoded = jwt.decode(token)
+
+            res.status(200).json({
+                message: `Welcome ${user.username}`,
+                token: token,
+                id: decoded.id
+            })
+        })
+        .catch(err =>{
+            res.status(500).json(err)
+        })
+})
 
 module.exports = router
