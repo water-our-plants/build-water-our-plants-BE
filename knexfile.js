3e9652b6641e49de5233dfbe3249491ddbd8829f
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-07-09 22:13:42 -0700

    updated folders

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,17 @@
 module.exports = {
 
     development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './data/dev.sqlite3'
+        client: 'sqlite3',
+        connection: {
+          filename: './data/water-plants.db3'
+        },
+        useNullAsDefault: true,
+        migrations: {
+          directory: './data/migrations'
+        },
+        seeds: {
+          directory: './data/seeds'
+        }
       },
-      useNullAsDefault: true,
-    },
-
   };
   
