7202d440ded1256468832e88be8c5483a77191fb
Kristea Gamel <krgamel@gmail.com>
2019-07-11 22:02:21 -0500

    editing twilio sms

---
+++ data/seeds/03_watering.js
@@ -0,0 +1,10 @@
+exports.seed = async function(knex, Promise) {
+  return knex('plants')
+    //.truncate()
+    .then(function(){
+      return knex('watering').insert([
+        { plant_id:1 , watering_time: '2019-07-18' },
+        { plant_id:2 , watering_time: '2019-07-13' },
+      ]);
+    })
+};
